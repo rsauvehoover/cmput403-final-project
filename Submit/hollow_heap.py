e115b411153165e3c0b61613501581d0a6e5acab
rsauvehoover <rsauveho@ualberta.ca>
2020-04-20 23:27:09 -0600

    first submission

---
+++ Submit/hollow_heap.py
@@ -0,0 +1,176 @@
+"""
+Implementation based on the Hollow Heap based on paper by Hansen et al. https://arxiv.org/abs/1510.06535
+"""
+
+class HollowHeap:
+    def __init__(self):
+        # Full roots 
+        self.A = {}
+        self.h = None
+        self.max_rank = None
+
+    def find_min(self):
+        """find item with minimum key in h
+        :return: item with min key, null if h is empty
+        """
+        if self.h is None:
+            return None
+        return self.h.item
+
+    def insert(self, k, e = None):
+        """
+        Insert item e with key k. e must not be in h
+        :return: the heap with e added"""
+        if e is None:
+            e = Element()
+        self.meld(_HollowHeapNode(e, k))
+        return self.h
+
+    def delete_min(self):
+        """
+        delete the mimimum item of the heap
+        :return: the key of the deleted item, None if the heap is empty
+        """
+        val = self.min_key()
+        if val is not None:
+            self.delete(self.h.item)
+        return val
+
+    def meld(self, g):
+        """
+        melds g into self.h, where g and self.h have distinct keys
+        """
+        if g is None: 
+            return
+        if self.h is None: 
+            self.h = g
+            return 
+        self.h = _HollowHeapNode._link(self.h, g)
+
+    def decrease_key(self, e, k):
+        """
+        changes the key of element e to k, if e originally has a key larger than k
+        """
+        u = e.node
+        if u == self.h:
+            u.key = k
+            return
+        v = _HollowHeapNode(e, k)
+        u.item = None
+        if u.rank > 2:
+            v.rank = u.rank - 2
+        v.child = u
+        u.ep = v
+        self.h = _HollowHeapNode._link(self.hm, v)
+
+    def delete(self, e):
+        """
+        delete e from heap h
+        """
+        e.node.item = None
+        e.node = None
+
+        # if we're deleting something other than our min, then we don't need
+        # to do anything else
+        if self.h is not None and self.h.item is not None:
+            return
+
+        # we need to reset A and max_rank
+        self.max_rank = 0
+        self.A = {}
+        while self.h is not None:
+            w = self.h.child
+            v = self.h
+            self.h = self.h.next
+            
+            while w is not None:
+                u = w
+                w = w.next
+                if u.item is None:
+                    if u.ep is None:
+                        u.next = self.h
+                        self.h = u
+                    else:
+                        if u.ep == v:
+                            w = None
+                        else:
+                            u.next = None
+                        u.ep = None
+                else:
+                    self._do_ranked_links(u)
+        self._do_unranked_links()
+    
+    def _do_ranked_links(self, u):
+        """
+        link nodes based on rankings
+        """
+        while u.rank in self.A and self.A[u.rank] is not None:
+            u = _HollowHeapNode._link(u, self.A[u.rank])
+            self.A[u.rank] = None
+            u.rank += 1
+        self.A[u.rank] = u
+        self.max_rank = max(self.max_rank, u.rank)
+
+    def _do_unranked_links(self):
+        for i in range(self.max_rank + 1):
+            if i in self.A and self.A[i] is not None:
+                if self.h is None:
+                    self.h = self.A[i]
+                else:
+                    self.h = _HollowHeapNode._link(self.h, self.A[i])
+                self.A[i] = None
+
+    def min_key(self):
+        """
+        returns the integer value of the minimum key in the heap
+        :return: key, or None if the heap is empty
+        """
+        m = self.find_min()
+        if m is None:
+            return None
+        else:
+            return m.node.key
+
+
+class _HollowHeapNode:
+    """
+    The node data structure
+    """
+    def __init__(self, e, k):
+        self.item = e
+        self.key = k
+        self.child = None
+        self.next = None
+        self.ep = None
+        self.rank = 0
+        e.node = self
+
+    def _add_child(v, w):
+        """
+        Auxillary function,
+        adds v as a child of w
+        """
+        v.next = w.child
+        w.child = v
+
+    def _link(v, w):
+        """
+        Auxillary function,
+        makes the node with the larger key a child of the smaller
+        :return: the smaller node
+        """
+        if v.key >= w.key:
+            _HollowHeapNode._add_child(v, w)
+            return w
+        else:
+            _HollowHeapNode._add_child(w, v)
+            return v
+
+class Element:
+    """
+    Element data structure, contains only a reference to the node containing it
+    """
+    def __init__(self, data=None):
+        self.node = None
+        # whatever data you want to store in it, should be a pointer to some object
+        self.data = data
---
+++ Submit/tests.py
@@ -0,0 +1,148 @@
+import pytest
+from hollow_heap import Element, HollowHeap
+
+def test_insert():
+    a = HollowHeap()
+    # h should be null when intialized
+    assert a.h is None
+
+    a.insert(3)
+    # item of h should exist now, and be equal to our newly added key
+    assert a.h.item is not None
+    assert a.h.item.node.key == 3
+
+    a.insert(4)
+    # inserting a larger key should keep h.item the same
+    assert a.h.item.node.key == 3
+
+    a.insert(2)
+    # inserting a smaller key should update h.item
+    assert a.h.item.node.key == 2
+
+
+    # run the same tests passing in en Element()
+    a = HollowHeap()
+    # h should be null when intialized
+    assert a.h is None
+
+    a.insert(3, Element())
+    # item of h should exist now, and be equal to our newly added key
+    assert a.h.item is not None
+    assert a.h.item.node.key == 3
+
+    el = Element()
+    a.insert(4, el)
+    # inserting a larger key should keep h.item the same
+    assert a.h.item.node.key == 3
+
+    a.insert(2, Element)
+    # inserting a smaller key should update h.item
+    assert a.h.item.node.key == 2
+
+def test_find_min_key():
+    a = HollowHeap()
+
+    a.insert(3)
+    assert a.min_key() == 3
+
+    a.insert(4)
+    assert a.min_key() == 3
+
+    a.insert(2)
+    assert a.min_key() == 2
+
+def test_delete_min():
+    a = HollowHeap()
+
+    a.insert(3)
+    a.insert(4)
+    a.insert(2)
+
+    assert a.min_key() == 2
+    assert a.delete_min() == 2
+
+    assert a.min_key() == 3
+    assert a.delete_min() == 3
+
+    assert a.min_key() == 4
+    assert a.delete_min() == 4
+
+    assert a.min_key() is None
+    assert a.delete_min() is None
+
+    assert a.min_key() is None
+
+def test_delete():
+    a = HollowHeap()
+
+    e = Element()
+    a.insert(3, e)
+    a.insert(2)
+
+    a.delete(e)
+    assert a.min_key() == 2
+
+    a.delete_min()
+
+    assert a.min_key() is None
+
+def test_meld():
+    a = HollowHeap()
+    b = HollowHeap()
+
+    a.insert(4)
+    b.insert(3)
+
+    assert a.min_key() == 4
+    assert b.min_key() == 3
+
+    a.meld(b.h)
+
+    assert a.min_key() == 3
+    assert b.min_key() == 3
+    
+    a = HollowHeap()
+    b = HollowHeap()
+
+    a.insert(4)
+    b.insert(3)
+
+    assert a.min_key() == 4
+    assert b.min_key() == 3
+
+    b.meld(a.h)
+
+    assert a.min_key() == 4
+    assert b.min_key() == 3
+
+def test_decrease_key():
+    a = HollowHeap()
+
+    a.insert(4)
+
+    assert a.min_key() == 4
+
+    a.decrease_key(a.find_min(), 3)
+
+    assert a.min_key() == 3
+
+    a.decrease_key(a.find_min(), 0)
+
+    assert a.min_key() == 0
+
+def test_element_data():
+    a = HollowHeap()
+
+    e = Element([1,2,3])
+
+    a.insert(3, e)
+
+    assert a.find_min().data == [1,2,3]
+
+    a.insert(2)
+
+    assert a.find_min().data != [1,2,3]
+
+    a.delete_min()
+
+    assert a.find_min().data == [1,2,3]
